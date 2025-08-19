79192ac70017f9e25d08629c56ff2f844d0a32ee
Ben Noetzel-Kiers <167789286+bkiers98@users.noreply.github.com>
2025-08-19 01:49:56 +0000

    Edit .js and add button to .html
    Add hover functionality and alternate class to the grid, and a CTA button

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
+const startButton = document.querySelector("#start");
 const gridField = document.querySelector(".grid-field");
-const divider = 16;
+let sideLength;
 
-for(let i = 0; i < 16; i++) {
-    const column = document.createElement("div");
-    column.setAttribute("class", "column");
-    gridField.appendChild(column);
+startButton.addEventListener("click", () => {
+    sideLength = Number(prompt("Enter a number to determine the side length of the grid"));
+    if(sideLength > 100) {
+        sideLength = Number(prompt("Please enter a number less than 100"));
+    };
 
-    for(let j = 0; j < 16; j++) {
-        const row = document.createElement("div");
-        row.setAttribute("class", "row");
-        column.appendChild(row);
-    }
-};
+    gridField.innerHTML = "";
+
+    for(let i = 0; i < sideLength; i++) {
+        const column = document.createElement("div");
+        column.setAttribute("class", "column");
+        gridField.appendChild(column);
+
+        for(let j = 0; j < sideLength; j++) {
+            const row = document.createElement("div");
+            row.setAttribute("class", "row");
+            row.addEventListener("mouseenter", () => {
+                row.setAttribute("class", "row-hovered");
+            });
+            column.appendChild(row);
+        }
+    };
+
+});
+
+
+
+
